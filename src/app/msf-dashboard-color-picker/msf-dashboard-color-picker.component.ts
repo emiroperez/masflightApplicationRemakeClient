5cc4a487570e5baddcebd2f37c13a50eace9d09a
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-03-14 18:06:20 -0500

    - Added a new package that allows to select a color. - The charts displayed on the dashboard now use colors in which the user can customize for their liking. - Tweaked the dialogs used on the dashboard in order to avoid subtle appearance issues such as visible scrollbars when they shouldn't.

--- src/app/msf-dashboard-color-picker/msf-dashboard-color-picker.component.ts
+++ src/app/msf-dashboard-color-picker/msf-dashboard-color-picker.component.ts
@@ -3,13 +3,13 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Globals } from '../globals/Globals';
 
 @Component({
-  selector: 'app-msf-dashboard-control-variables',
-  templateUrl: './msf-dashboard-control-variables.component.html'
+  selector: 'app-msf-dashboard-color-picker',
+  templateUrl: './msf-dashboard-color-picker.component.html'
 })
-export class MsfDashboardControlVariablesComponent {
+export class MsfDashboardColorPickerComponent {
 
   constructor(
-    public dialogRef: MatDialogRef<MsfDashboardControlVariablesComponent>,
+    public dialogRef: MatDialogRef<MsfDashboardColorPickerComponent>,
     public globals: Globals,
     @Inject(MAT_DIALOG_DATA) public data: any) {}
 
