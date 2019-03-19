fd86db29ee7f85851afda3df65b41a97a99e768f
Marbel <marbelarrietam@gmail.com>
2019-03-19 17:13:46 -0500

    adding button add dashboard

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -55,6 +55,15 @@ export class MenuService {
       this.get (_this, _this.globals.baseUrl + "/secure" + url, successHandler, errorHandler);
   }
 
+  addDashboard(_this, data, successHandler, errorHandler){
+    let url = "/addDashboardMenu";
+    if (_this.globals.baseUrl != ""){
+        this.post (_this,  _this.globals.baseUrl + url,data, successHandler, errorHandler);
+    }else{
+      this.post (_this, _this.globals.baseUrl + "/secure" + url, data, successHandler, errorHandler);
+}
+  }
+
   updateDashboardTitle(_this, id, title, successHandler, errorHandler)
   {
     let url = _this.globals.baseUrl+ "/updateDashboardTitle?id=" + id + "&title=" + title;
@@ -78,7 +87,7 @@ export class MenuService {
   post = function (_this,url, data, successHandler, errorHandler) {
     this.http.post(url, data).subscribe(result => {
         successHandler(_this,result);
-      }, error => 
+      }, error =>
       errorHandler(_this,error)
     );
   }
