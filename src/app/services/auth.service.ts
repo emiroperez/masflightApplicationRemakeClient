a52d9bc415f654ae7ebf03a91840f015986972e3
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-05-10 09:15:00 -0500

    Move authorization-related routines into auth.service.ts.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,54 @@
 import { Injectable } from '@angular/core';
-import {ApiClient} from '../api/api-client';
 import { Globals } from '../globals/Globals';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+const SECURITY_HEADER = "Authorization";
+const TOKEN_STORAGE_KEY = "token";
 
 @Injectable()
 export class AuthService {
-  constructor(private http: ApiClient, private globals:Globals) { 
+  constructor(private http: HttpClient, private globals:Globals)
+  { 
   }
 
-  login(_this,credentials,successHandler, errorHandler){
+  login (_this, credentials, successHandler, errorHandler)
+  {
     // let url = 'http://localhost:8887/login';
     let url = this.globals.baseUrl+'/login';
-    this.http.post(_this, url,credentials,successHandler, errorHandler);
+    this.post (_this, url, credentials, successHandler, errorHandler);
+  }
+
+  createAuthorizationHeader()
+  {
+    let token = localStorage.getItem ('token');
+
+    if (!token)
+      token = "noUser";
+
+    httpOptions.headers = new HttpHeaders ({ 'Content-Type': 'application/json' });
+    // httpOptions.headers = httpOptions.headers.append (SECURITY_HEADER, localStorage.getItem (TOKEN_STORAGE_KEY));
+    httpOptions.headers = httpOptions.headers.append ('Authorization', token);
+  }
+
+  get = function (_this, url, successHandler, errorHandler)
+  {
+    this.createAuthorizationHeader ();
+    this.http.get (url, httpOptions).subscribe (result => {
+        successHandler (_this, result);
+      }, error => errorHandler (_this, error)
+    );
+  }
+
+  post = function (_this, url, data, successHandler, errorHandler)
+  {
+    this.createAuthorizationHeader ();
+    this.http.post (url, data, httpOptions).subscribe (result => {
+        successHandler (_this, result);
+      }, error => errorHandler (_this, error)
+    );
   }
 }
--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,21 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ApiClient } from '../api/api-client';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Menu } from '../model/Menu';
-import { Globals } from '../globals/Globals';
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class MenuService {
-  SECURITY_HEADER = "Authorization";
-  TOKEN_STORAGE_KEY = "token";
-  constructor( private http: HttpClient, private globals:Globals) {
+  constructor(private authService: AuthService) {
   }
 
   getMenu(_this,successHandler, errorHandler){
@@ -26,141 +17,81 @@ export class MenuService {
     url = url + "application="+_this.globals.currentApplication.id;
 
     _this.globals.isLoading = true;
-    // if (_this.globals.baseUrl != "")
-      // this.get (_this, _this.globals.baseUrl + url, successHandler, errorHandler);
-    // else
-      this.get (_this, _this.globals.baseUrl + "/secure" + url, successHandler, errorHandler);
+    this.authService.get (_this, _this.globals.baseUrl + "/secure" + url, successHandler, errorHandler);
   }
 
   getAdvanceFeatures(_this, successHandler, errorHandler){
     let url = "/getPlanAdvanceFeatures";
-    // if (_this.globals.baseUrl != "")
-    //   this.get (_this, _this.globals.baseUrl + url, successHandler, errorHandler);
-    // else
-      this.get (_this, _this.globals.baseUrl + "/secure" + url, successHandler, errorHandler);
+    this.authService.get (_this, _this.globals.baseUrl + "/secure" + url, successHandler, errorHandler);
   }
 
   getUserLoggedin(_this,successHandler, errorHandler){
     let url = _this.globals.baseUrl+ "/secure/getUserloggedin";
     //let url = "http://localhost:8887/secure/getUserloggedin"
-    this.get(_this, url, successHandler, errorHandler);
+    this.authService.get(_this, url, successHandler, errorHandler);
   }
 
   getDashboardsByUser(_this, successHandler, errorHandler){
     let url = "/getDashboards?application=" + _this.globals.currentApplication.id;
-
-    // if (_this.globals.baseUrl != "")
-    //   this.get (_this, _this.globals.baseUrl + url, successHandler, errorHandler);
-    // else
-      this.get (_this, _this.globals.baseUrl + "/secure" + url, successHandler, errorHandler);
+    this.authService.get (_this, _this.globals.baseUrl + "/secure" + url, successHandler, errorHandler);
   }
 
   addDashboard(_this, data, successHandler, errorHandler){
     let url = "/addDashboardMenu";
-    // if (_this.globals.baseUrl != ""){
-    //     this.post (_this,  _this.globals.baseUrl + url,data, successHandler, errorHandler);
-    // }else{
-      this.postSecure (_this, _this.globals.baseUrl + "/secure" + url, data, successHandler, errorHandler);
-// }
+    this.authService.post (_this, _this.globals.baseUrl + "/secure" + url, data, successHandler, errorHandler);
   }
 
   updateDashboardTitle(_this, id, title, successHandler, errorHandler)
   {
     let url = _this.globals.baseUrl + "/updateDashboardTitle?id=" + id + "&title=" + title;
-    this.post (_this, url, null, successHandler, errorHandler);
+    this.authService.post (_this, url, null, successHandler, errorHandler);
   }
 
   deleteDashboard(_this, id, successHandler, errorHandler)
   {
     let url = _this.globals.baseUrl + "/deleteDashboard?id=" + id;
-    this.post (_this, url, null, successHandler, errorHandler);
+    this.authService.post (_this, url, null, successHandler, errorHandler);
   }
 
   deleteSharedDashboard(_this, dashboardId, successHandler, errorHandler)
   {
     let url = _this.globals.baseUrl + "/secure/deleteSharedDashboard?dashboardId=" + dashboardId;
-    this.postSecure (_this, url, null, successHandler, errorHandler);
+    this.authService.post (_this, url, null, successHandler, errorHandler);
   }
 
   getSharedContentByUser(_this, successHandler, errorHandler)
   {
     let url = _this.globals.baseUrl + "/secure/getSharedContent/byUser?appId=" + _this.globals.currentApplication.id;
-    this.get (_this, url, successHandler, errorHandler);
+    this.authService.get (_this, url, successHandler, errorHandler);
   }
 
   addSharedDashboard(_this, dashboardId, handlerSuccess, handlerError)
   {
     let url = _this.globals.baseUrl + "/secure/addSharedDashboard?dashboardId=" + dashboardId;
-    this.postSecure (_this, url, null, handlerSuccess, handlerError);
+    this.authService.post (_this, url, null, handlerSuccess, handlerError);
   }
 
   addSharedReadOnlyDashboard(_this, dashboardId, handlerSuccess, handlerError)
   {
     let url = _this.globals.baseUrl + "/secure/addSharedReadOnlyDashboard?dashboardId=" + dashboardId;
-    this.postSecure (_this, url, null, handlerSuccess, handlerError);
+    this.authService.post (_this, url, null, handlerSuccess, handlerError);
   }
 
   getSharedDashboardsByUser(_this, successHandler, errorHandler)
   {
     let url = _this.globals.baseUrl + "/secure/getSharedDashboards?application=" + _this.globals.currentApplication.id;
-    this.get (_this, url, successHandler, errorHandler);
+    this.authService.get (_this, url, successHandler, errorHandler);
   }
 
   getUsers(_this, handlerSuccess, handlerError)
   {
     let url = _this.globals.baseUrl + "/secure/getAllUsers";
-    this.get (_this, url, handlerSuccess, handlerError);
+    this.authService.get (_this, url, handlerSuccess, handlerError);
   }
 
   addSharedContent(_this, sharedContent, handlerSuccess, handlerError)
   {
     let url = _this.globals.baseUrl + "/secure/addSharedContent";
-    this.postSecure (_this, url, sharedContent, handlerSuccess, handlerError);
+    this.authService.post (_this, url, sharedContent, handlerSuccess, handlerError);
   }
-
-  createAuthorizationHeader() {
-    let token = localStorage.getItem('token');
-
-    if (!token)
-      token = "noUser";
-
-    httpOptions.headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    // httpOptions.headers = httpOptions.headers.append(this.SECURITY_HEADER, localStorage.getItem(this.TOKEN_STORAGE_KEY));
-    httpOptions.headers = httpOptions.headers.append('Authorization', token);
-  }
-
-  get = function (_this,url,successHandler, errorHandler){
-    this.createAuthorizationHeader();
-    this.http.get(url,httpOptions).subscribe(result => {
-        successHandler(_this,result);
-    }, error =>
-        errorHandler(_this,error)
-  );
-  }
-
-  post = function (_this,url, data, successHandler, errorHandler) {
-    this.http.post(url, data).subscribe(result => {
-        successHandler(_this,result);
-      }, error =>
-      errorHandler(_this,error)
-    );
-  }
-
-  postSecure = function (_this,url, data, successHandler, errorHandler) {
-    this.createAuthorizationHeader();
-    this.http.post(url, data,httpOptions).subscribe(result => {
-        successHandler(_this,result);
-      }, error =>
-      errorHandler(_this,error)
-    );
-  }
-
-  // get = function (_this,url,successHandler, errorHandler){
-  //   this.http.get(url).subscribe(result => {
-  //       successHandler(_this,result);
-  //   }, error =>
-  //       errorHandler(_this,error)
-  // );
-  // }
-
 }
