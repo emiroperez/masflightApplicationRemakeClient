ef8591c6423b3ac04d53eb3341acc65ed03b6bd2
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-05-17 16:55:37 -0500

    Renamed machineName to hash.

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -105,7 +105,7 @@ export class LoginScreenComponent implements OnInit {
           self.session = {
             userId: _this.userId,
             ipAddress: data["ip"],
-            machineName: hash
+            hash: hash
           };
 
           _this.authService.validateLogin (self, self.session, self.verifyLogin, self.errorAutentication);
