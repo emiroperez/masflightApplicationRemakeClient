2dfc7e995962d65625ab744cd712575d73b02525
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-06-13 17:30:15 -0500

    When the mapbox panel is used, only display the options that supports this functionality.

--- src/app/msf-dashboard-panel/msf-dashboard-panel-option-pipe.ts
+++ src/app/msf-dashboard-panel/msf-dashboard-panel-option-pipe.ts
@@ -11,7 +11,7 @@ export class MsfDashboardPanelOptionPipe implements PipeTransform
     transform(items: any[], currentChartType: any): any[]
     {
         // filter out options that doesn't use the schedule maps if the map panel type is selected
-        return items.filter (item => ((item.metaData == 2 && currentChartType.flags & ChartFlags.MAP)
-            || !(currentChartType.flags & ChartFlags.MAP) || (currentChartType.flags & ChartFlags.MAPBOX)));
+        return items.filter (item => ((item.metaData == 2 && currentChartType.flags & ChartFlags.MAP && !(currentChartType.flags & ChartFlags.MAPBOX))
+            || !(currentChartType.flags & ChartFlags.MAP) || (item.tabType === 'map' && currentChartType.flags & ChartFlags.MAPBOX)));
     }
 }
