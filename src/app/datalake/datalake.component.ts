c352ceed583ac7f11fab0694a874d6d546bd5a08
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-08-14 15:32:02 -0500

    Pulse header for the datalake application.

--- src/app/datalake/datalake.component.ts
+++ src/app/datalake/datalake.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+
+import { Globals } from '../globals/Globals';
+import { ApplicationService } from '../services/application.service';
+import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { MenuService } from '../services/menu.service';
 
 @Component({
   selector: 'app-datalake',
@@ -6,9 +15,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatalakeComponent implements OnInit {
 
-  constructor() { }
+  TabletQuery: MediaQueryList;
+  mobileQuery: MediaQueryList;
+  ResponsiveQuery: MediaQueryList;
+  private _TabletQueryListener: () => void;
+  private _mobileQueryListener: () => void;
+  private _ResponsiveQueryListener: () => void; 
 
-  ngOnInit() {
+  constructor(public globals: Globals, private appService: ApplicationService, private changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher, private router: Router, public authGuard: AuthGuard, public authService: AuthService,
+    public userService: UserService, private menuService: MenuService)
+  {
+    this.TabletQuery = media.matchMedia('(max-width: 768px)');
+    this._TabletQueryListener = () => changeDetectorRef.detectChanges ();
+    this.TabletQuery.addListener (this._TabletQueryListener);
+    
+    this.mobileQuery = media.matchMedia('(max-width: 480px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges ();
+    this.mobileQuery.addListener (this._mobileQueryListener);
+
+    this.ResponsiveQuery = media.matchMedia('(max-width: 759px)');
+    this._ResponsiveQueryListener = () => changeDetectorRef.detectChanges ();
+    this.ResponsiveQuery.addListener (this._ResponsiveQueryListener);
   }
 
+  ngOnInit()
+  {
+    this.globals.isLoading = true;
+    this.menuService.getUserLoggedin (this, this.handleLogin, this.errorLogin);
+  }
+
+  ngOnDestroy(): void
+  {
+    this.TabletQuery.removeListener (this._TabletQueryListener);
+	  this.mobileQuery.removeListener (this._mobileQueryListener);
+	  this.ResponsiveQuery.removeListener (this._ResponsiveQueryListener);
+  }
+
+  getMenuVisibility(): string
+  {
+    if (!this.mobileQuery.matches && !this.ResponsiveQuery.matches)
+      return "block";
+
+    return "none";
+  }
+
+  goHome(): void
+  {
+    this.router.navigate (["/welcome"]);
+  }
+
+  logOut(): void
+  {
+    this.appService.confirmationDialog (this, "Are you sure you want to Log Out?",
+      function (_this)
+      {
+        _this.userService.setUserLastLoginTime (_this, _this.logoutSuccess, _this.logoutError);
+      });
+  }
+  
+  logoutSuccess(_this): void
+  {
+    _this.authGuard.disableSessionInterval ();
+    _this.authService.removeToken ();
+    _this.router.navigate (['']);
+  }
+
+  logoutError(_this, error): void
+  {
+    console.log (error);
+    _this.authGuard.disableSessionInterval ();
+    _this.authService.removeToken ();
+    _this.router.navigate (['']);
+  }
+
+  optionHandler(): void
+  {
+    this.changeDetectorRef.detectChanges ();
+  }
+
+  handleLogin(_this, data): void
+  {
+    _this.globals.currentUser = data.name;
+    _this.globals.isLoading = false;
+  }
+
+  errorLogin(_this, result): void
+  {
+    console.log(result);
+    _this.globals.isLoading = false;
+  }
 }
