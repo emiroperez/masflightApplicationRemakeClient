62bf69cabda38186a0475a4b88073c8f7db334bc
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-08-15 15:02:46 -0500

    Buttons for the Datalake table cards.

--- src/app/datalake-table-card/datalake-table-card.component.ts
+++ src/app/datalake-table-card/datalake-table-card.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Globals } from '../globals/Globals';
+
 @Component({
   selector: 'app-datalake-table-card',
   templateUrl: './datalake-table-card.component.html'
 })
 export class DatalakeTableCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public globals: Globals) { }
 
   ngOnInit() {
   }
