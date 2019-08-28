7271769de9118ca1e8c4f8b8d67b857d892731e0
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-08-28 11:28:56 -0500

    The mat-step appareance is now complete.

--- src/app/datalake-create-table/datalake-create-table.component.ts
+++ src/app/datalake-create-table/datalake-create-table.component.ts
@@ -1,16 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
+import { Component } from '@angular/core';
+import { MatDialogRef, MatStepper } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-datalake-create-table',
   templateUrl: './datalake-create-table.component.html'
 })
 export class DatalakeCreateTableComponent {
+  newStep1FormGroup: FormGroup;
+  newStep2FormGroup: FormGroup;
+  newStep3FormGroup: FormGroup;
+  newStep4FormGroup: FormGroup;
+  uploadStep1FormGroup: FormGroup;
+  uploadStep2FormGroup: FormGroup;
+  uploadStep3FormGroup: FormGroup;
 
-  constructor(public dialogRef: MatDialogRef<DatalakeCreateTableComponent>) { }
+  constructor(public dialogRef: MatDialogRef<DatalakeCreateTableComponent>,
+    private formBuilder: FormBuilder)
+  {
+    this.newStep1FormGroup = this.formBuilder.group ({
+      firstCtrl: ['']
+    });
+    this.newStep2FormGroup = this.formBuilder.group ({
+      secondCtrl: ['']
+    });
+    this.newStep3FormGroup = this.formBuilder.group ({
+      secondCtrl: ['']
+    });
+    this.newStep4FormGroup = this.formBuilder.group ({
+      secondCtrl: ['', Validators.required]
+    });
+
+    this.uploadStep1FormGroup = this.formBuilder.group ({
+      secondCtrl: ['']
+    });
+    this.uploadStep2FormGroup = this.formBuilder.group ({
+      secondCtrl: ['']
+    });
+    this.uploadStep3FormGroup = this.formBuilder.group ({
+      secondCtrl: ['', Validators.required]
+    });
+  }
 
   onNoClick(): void
   {
     this.dialogRef.close ();
+  }
+
+  goBack(stepper: MatStepper): void
+  {
+    stepper.previous ();
+  }
+
+  goForward(stepper: MatStepper): void
+  {
+    stepper.next ();
   }
 }
