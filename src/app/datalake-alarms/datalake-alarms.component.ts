addae87d7b71e93c6eef8d3ae14c6b0786d12f60
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-09-09 10:46:39 -0500

    Right side of the alarm menu mockup improvement. Except for the time picker.

--- src/app/datalake-alarms/datalake-alarms.component.ts
+++ src/app/datalake-alarms/datalake-alarms.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-datalake-alarms',
   templateUrl: './datalake-alarms.component.html'
 })
 export class DatalakeAlarmsComponent implements OnInit {
+  alarmFormGroup: FormGroup;
+  notifyMode: number = 0;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.alarmFormGroup = this.formBuilder.group ({
+      schema: ['', Validators.required],
+      table: new FormControl ({ value: '', disabled: true }, Validators.required)
+    });
+  }
 
   ngOnInit() {
+  }
+
+  schemaChanged(): void
+  {
   }
 
 }
