f55b286b09dc7bb1554a0d19ae00d0175b595e68
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-10-02 09:56:34 -0500

    Fix for date period year month date value.

--- src/app/msf-date-period-year-month/msf-date-period-year-month.component.ts
+++ src/app/msf-date-period-year-month/msf-date-period-year-month.component.ts
@@ -56,34 +56,34 @@ export class MsfDatePeriodYearMonthComponent implements OnInit {
     }
     if(!this.argument.value1&&!this.argument.value2){
     this.argument.value1 = this.date.value.year();
-    this.argument.value2 = this.date.value.month();
+    this.argument.value2 = this.date.value.month() + 1;
     }
     else if (this.argument.value1)
     {
       let ctrlValue;
-  
+
       switch (this.argument.value1)
       {
         case "CURRENTYEAR":
         default:
           this.argument.value1 = this.date.value.year ();
-          this.argument.value2 = this.date.value.month ();
+          this.argument.value2 = this.date.value.month () + 1;
           break;
 
         case 'LASTMONTH':
           this.argument.value1 = this.date.value.year ();
-          this.argument.value2 = this.date.value.month () - 1;
+          this.argument.value2 = this.date.value.month ();
           break;
   
         case 'LASTYEAR':
           this.argument.value1 = this.date.value.year () - 1;
-          this.argument.value2 = this.date.value.month ();
+          this.argument.value2 = this.date.value.month () + 1;
           break;
       }
-  
+
       ctrlValue = this.date.value;
       ctrlValue.year (this.argument.value1);
-      ctrlValue.month (this.argument.value2);
+      ctrlValue.month (this.argument.value2 - 1);
       this.date.setValue (ctrlValue);
     }
 
