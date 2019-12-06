d41230a0c74ec09423041bdb5a3770c0c29d7020
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2019-12-06 14:41:33 -0500

    Move the selected DataLake option when loading is finished.

--- src/app/datalake/datalake.component.ts
+++ src/app/datalake/datalake.component.ts
@@ -116,7 +116,7 @@ export class DatalakeComponent implements OnInit {
     _this.globals.currentUser = data.name;
     _this.globals.admin = data.admin;
     _this.globals.userName = data.email;
-    _this.globals.optionDatalakeSelected = 2;
+
     if(data.userInfoDatalake){
       data.userInfoDatalake.datalakeRoles.forEach(datalakeRole => {
         datalakeRole.role.datalakeOption.forEach(datalakeOption => {
@@ -186,12 +186,14 @@ export class DatalakeComponent implements OnInit {
 
       if (!_this.globals.currentDashboardMenu)
       {
+        _this.globals.optionDatalakeSelected = 2;
         _this.globals.appLoading = false;
         _this.globals.isLoading = false;
       }
     }
     else
     {
+      _this.globals.optionDatalakeSelected = 2;
       _this.defaultDashboardId = null;
       _this.globals.appLoading = false;
       _this.globals.isLoading = false;
