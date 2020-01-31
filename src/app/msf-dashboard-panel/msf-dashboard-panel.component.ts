a7c7da047a9b3a22589f7a40bc35c9cf68b875c5
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2020-01-31 17:09:58 -0500

    Removed height from dashboard panel control variables dialog.

--- src/app/msf-dashboard-panel/msf-dashboard-panel.component.ts
+++ src/app/msf-dashboard-panel/msf-dashboard-panel.component.ts
@@ -3801,7 +3801,6 @@ export class MsfDashboardPanelComponent implements OnInit {
     this.toggleControlVariableDialogOpen.emit (true);
 
     dialogRef = this.dialog.open (MsfDashboardControlVariablesComponent, {
-      height: '605px',
       width: '400px',
       panelClass: 'msf-dashboard-arguments-dialog',
       data: {
