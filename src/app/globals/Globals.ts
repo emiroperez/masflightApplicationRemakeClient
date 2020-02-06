5ec2eb3784bc921aec7675095b003260f7b8fb60
karen.perez <karen.perez@aspsols.com>
2020-02-06 09:15:02 -0500

    showMoreResult add

--- src/app/globals/Globals.ts
+++ src/app/globals/Globals.ts
@@ -62,8 +62,8 @@ export class Globals {
   template : boolean = false;
   isFullscreen: boolean = false;
   // baseUrl = "http://localhost:8887";
-  baseUrl = "http://pulse.globaleagle.com:8887";
-  // baseUrl = "";
+  // baseUrl = "http://pulse.globaleagle.com:8887";
+  baseUrl = "";
   baseUrl2 = "https://pulse.globaleagle.com:8886/mapBoxServices";
   popupUrl = "https://pulse.globaleagle.com:8900";
   scheduledata:any;
@@ -109,8 +109,8 @@ export class Globals {
   optionsDatalake: any = [];
   // userName: string = "eric.haag.t@aspsols.com";
   userName: string = "";
-  showPaginator = false;
-  showMoreResult: boolean;
+  showPaginator: boolean = false;
+  showMoreResult: boolean = false;
 
   constructor (public overlayContainer: OverlayContainer, private cookie: Cookie)
   {
@@ -293,6 +293,7 @@ clearVariablesMenu(){
     this.tab = false;
     this.hideParametersPanels = false;
     this.showPaginator = false
+    this.showMoreResult = false;
   }
 
   getTime(){
