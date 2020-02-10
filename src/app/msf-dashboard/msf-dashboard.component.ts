b84f64fe4d78785bc8244268a84457f7dc6cdef8
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2020-02-10 16:54:28 -0500

    Chart goals can now be saved and loaded.

--- src/app/msf-dashboard/msf-dashboard.component.ts
+++ src/app/msf-dashboard/msf-dashboard.component.ts
@@ -316,7 +316,7 @@ export class MsfDashboardComponent implements OnInit {
         dashboardPanel.horizAxisName, dashboardPanel.advIntervalValue,
         dashboardPanel.startAtZero, dashboardPanel.limitMode,
         dashboardPanel.limitAmount, dashboardPanel.ordered,
-        dashboardPanel.valueList));
+        dashboardPanel.valueList, dashboardPanel.goals));
     }
 
     // add the last dashboard column
