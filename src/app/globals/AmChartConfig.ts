c8d8c9233bdaa6d953b13b7e6f298bdd14c9426e
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2020-04-16 09:33:52 -0500

    Disable the AmChart viewport optimizations.

--- src/app/globals/AmChartConfig.ts
+++ src/app/globals/AmChartConfig.ts
@@ -5,7 +5,7 @@ export class AmChartConfig
     public static Init(): void
     {
         // set some optimizations to avoid slowdowns
-        am4core.options.queue = true;
-        am4core.options.onlyShowOnViewport = true;
+        //am4core.options.queue = true;
+        //am4core.options.onlyShowOnViewport = true;
     }
 }
