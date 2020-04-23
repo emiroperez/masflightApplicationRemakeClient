ec362c6be1528cf650f8d94c08d69111dd801a99
gmlb256 <47115929+gmlb256@users.noreply.github.com>
2020-04-23 15:14:53 -0500

    Add inactivity timeout.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -6,14 +6,38 @@ import { MessageComponent } from '../message/message.component';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  static INACTIVITY_TIMEOUT: number = 30 * 60 * 1000; // 30 minutes for inactivity timeout
+
   sessionInterval: any;
+  inactivityTimeout: any;
+  timeoutLogout: boolean = false;
 
   constructor (private router: Router, private authService: AuthService, private dialog: MatDialog)
   {
   }
 
-  disableSessionInterval()
+  private clearIntervals(): void
   {
+    let _this = this;
+
+    if (this.inactivityTimeout)
+    {
+      clearTimeout (this.inactivityTimeout);
+      this.inactivityTimeout = null;
+
+      window.removeEventListener ("click", () => {
+        _this.resetInactivityTimeout ();
+      });
+
+      window.removeEventListener ("scroll", () => {
+        _this.resetInactivityTimeout ();
+      });
+
+      window.removeEventListener ("keypress", () => {
+        _this.resetInactivityTimeout ();
+      });
+    }
+
     if (this.sessionInterval)
     {
       clearInterval (this.sessionInterval);
@@ -21,8 +45,31 @@ export class AuthGuard implements CanActivate {
     }
   }
 
-  logout()
+  private resetInactivityTimeout(): void
   {
+    if (!this.inactivityTimeout)
+      return;
+
+    clearTimeout (this.inactivityTimeout);
+    this.inactivityTimeout = null;
+
+    this.inactivityTimeout = setTimeout (() => {
+      this.timeoutLogout = true;
+
+      this.logout();
+      this.clearIntervals ();
+    }, AuthGuard.INACTIVITY_TIMEOUT);
+  }
+
+  disableSessionInterval(): void
+  {
+    this.clearIntervals ();
+  }
+
+  logout(): void
+  {
+    let _this = this;
+
     if (this.authService.getToken ())
     {
       this.authService.setUserLastLoginTime (this, this.authService.getUserIdFromToken (), this.logoutSuccess, this.redirectToLogin);
@@ -32,37 +79,74 @@ export class AuthGuard implements CanActivate {
       this.redirectToLogin (this);
   }
 
-  logoutSuccess(_this)
+  logoutSuccess(_this): void
   {
-    const dialogRef = _this.dialog.open (MessageComponent, {
-      data: { title: "Session Expired", message: "Your session has expired. If you want to continue, please log in again." }
-    });
+    let dialogRef;
+
+    if (_this.timeoutLogout)
+    {
+      _this.timeoutLogout = false;
+
+      dialogRef = _this.dialog.open (MessageComponent, {
+        data: { title: "Timeout", message: "You've been logged out due to inactivity." }
+      });
+    }
+    else
+    {
+      dialogRef = _this.dialog.open (MessageComponent, {
+        data: { title: "Session Expired", message: "Your session has expired. If you want to continue, please log in again." }
+      });
+    }
   
     dialogRef.afterClosed ().subscribe (
       () => _this.redirectToLogin (_this)
     );
   }
 
-  redirectToLogin(_this)
+  redirectToLogin(_this): void
   {
     _this.router.navigate (['']);
   }
 
-  canActivate()
+  canActivate(): boolean
   {
     if (!this.authService.isTokenExpired ())
     {
+      if (!this.inactivityTimeout)
+      {
+        let _this = this;
+
+        window.addEventListener ("click", () => {
+          _this.resetInactivityTimeout ();
+        });
+
+        window.addEventListener ("scroll", () => {
+          _this.resetInactivityTimeout ();
+        });
+
+        window.addEventListener ("keypress", () => {
+          _this.resetInactivityTimeout ();
+        });
+
+        this.inactivityTimeout = setTimeout (() => {
+          this.timeoutLogout = true;
+
+          this.logout ();
+          this.clearIntervals ();
+        }, AuthGuard.INACTIVITY_TIMEOUT);
+      }
+
       if (!this.sessionInterval)
       {
+        let _this = this;
+
         // poll each 5 seconds while checking the status of the token
         this.sessionInterval = setInterval (() => {
           if (!this.authService.isTokenExpired ())
             return;
 
           this.logout ();
-
-          clearInterval (this.sessionInterval);
-          this.sessionInterval = null;
+          this.clearIntervals ();
         }, 5000);
       }
 
